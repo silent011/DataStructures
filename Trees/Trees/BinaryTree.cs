b1560146a74a9767c3c8ea5adfb02d5ac8e2663f
Silent-Era <junkyard@abv.bg>
2017-12-17 18:54:00 +0200

    Implementing tree structure with DFS and BFS traversals and binary tree with preorder, inorder and postorder logic

---
+++ Trees/Trees/BinaryTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public T Value { get; private set; }
+    public BinaryTree<T> Left { get; private set; }
+    public BinaryTree<T> Right { get; private set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        Value = value;
+        Left = leftChild;
+        Right = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + Value);
+        if(Left != null) Left.PrintIndentedPreOrder(indent + 2);
+        if(Right != null) Right.PrintIndentedPreOrder(indent + 2);
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (Left != null) Left.EachInOrder(action);
+        action(Value);
+        if (Right != null) Right.EachInOrder(action);
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (Left != null) Left.EachPostOrder(action);
+        if (Right != null) Right.EachPostOrder(action);
+        action(Value);
+    }
+}
---
+++ Trees/Trees/Tree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Tree<T>
+{
+    public T Value;
+    public List<Tree<T>> Children { get; private set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        Value = value;
+        Children = new List<Tree<T>>(children);
+    }
+
+    public void Print(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', indent) + Value);
+        foreach(var child in Children)
+        {
+            child.Print(indent + 2);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(Value);
+        foreach (var child in Children)
+        {
+            child.Each(action);
+        }
+    }
+
+    public IEnumerable<T> OrderDFS()
+    {
+       Queue<T> order = new Queue<T>();
+
+        DFS(order.Enqueue);
+
+        return order;
+    }
+
+    public void DFS(Action<T> action)
+    {
+        foreach (var child in Children)
+        {
+            child.DFS(action);
+        }
+        action(Value);
+    }
+
+    public IEnumerable<T> OrderBFS()
+    {
+        List<T> order = new List<T>();
+
+        Queue<Tree<T>> branchesQ = new Queue<Tree<T>>();
+        branchesQ.Enqueue(this);
+        order.Add(Value);
+        
+        while(branchesQ.Count > 0)
+        {
+            var current = branchesQ.Dequeue();
+
+            foreach (var child in current.Children)
+            {
+                branchesQ.Enqueue(child);
+                order.Add(child.Value);
+            }
+        }
+
+        return order;
+    }
+
+    
+}
