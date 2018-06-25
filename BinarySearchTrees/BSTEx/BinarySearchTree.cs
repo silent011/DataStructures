413150253a2d657cd045eaffea78851ce6c3e70f
Silent-Era <junkyard@abv.bg>
2018-06-25 05:23:28 +0300

    binary search tree basic functions

---
+++ BinarySearchTrees/BSTEx/BinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinarySearchTree<T> where T : IComparable
+{
+
+
+    public bool Contains(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(T value)
+    {
+
+    }
+}
+
+public class Launcher
+{
+    public static void Main()
+    {
+
+    }
+}
--- BinarySearchTrees/Trees/BinarySearchTree.cs
+++ BinarySearchTrees/Trees/BinarySearchTree.cs
@@ -7,104 +7,90 @@ public class BinarySearchTree<T> where T : IComparable<T>
 
     public BinarySearchTree()
     {
-
+        Root = null;
     }
 
-    private BinarySearchTree(Node node)
+    private BinarySearchTree(Node root)
     {
-        Copy(node);
+        Copy(root);
     }
 
-    private void Copy(Node node)
+    private void Copy(Node root)
     {
-        if(node == null)
-        {
-            return;
-        }
+        if (root == null) return;
 
-        Insert(node.Value);
-        Copy(node.Left);
-        Copy(node.Right);
+        Insert(root.Value);
+        Copy(root.Left);
+        Copy(root.Right);
     }
 
     public void Insert(T value)
     {
-        //if the binary tree does not have any nodes, node with this value shall be its first one
-        if(Root == null)
+        if (Root == null)
         {
             Root = new Node(value);
             return;
         }
 
-        bool isThere = false;
-        bool shouldBeLeft = false; 
-
-        Node parent = null;
         Node current = Root;
-
-        //looks for appropriate empty node and checks if the value is already in existence
-        while(current != null)
+        while (current != null)
         {
-            T val = current.Value;
-            if(value.CompareTo(val) < 0)
+            int valueComparison = ComparedTo(current.Value, value);
+            if (valueComparison > 0)
             {
-                if(current.Left == null)
+                if (current.Left == null)
                 {
-                    parent = current;
-                    shouldBeLeft = true;
+                    current.Left = new Node(value);
                     break;
                 }
-                else
-                    current = current.Left;
+
+                current = current.Left;
             }
-            else if(value.CompareTo(val) > 0)
+            else if (valueComparison < 0)
             {
                 if (current.Right == null)
                 {
-                    parent = current;
-                    break;
+                    current.Right = new Node(value);
                 }
-                else
-                    current = current.Right;
+
+                current = current.Right;
             }
             else
             {
-                isThere = true;
-                break;
+                Console.WriteLine("element already exists in the tree");
+                return;
             }
-        }
-
-        //if there isn't a node with the same value
-        if (!isThere)
-        {
-            if (shouldBeLeft)
-                parent.Left = new Node(value);
-            else
-                parent.Right = new Node(value);
         }
     }
 
-    public bool Contains(T value)
+    public BinarySearchTree<T> Search(T value)
     {
-        //nonrecursive implementation
-        bool isThere = false;
-
         Node current = Root;
-        while(current != null)
+        while (current != null)
         {
-            T currVal = current.Value;
-            if(value.CompareTo(currVal) > 0)
-                current = current.Right;
-            else if(value.CompareTo(currVal) < 0)
-                current = current.Left;
-            else
-            {
-                isThere = true;
-                break;
-            }
+            int valueCompared = ComparedTo(current.Value, value);
+            if (valueCompared > 0) current = current.Left;
+            else if (valueCompared < 0) current = current.Right;
+            else break;
         }
 
-        return isThere;
+        return new BinarySearchTree<T>(current);
+    }
+
+
+    public bool Contains(T value)
+    {
+        Node current = Root;
+        while (current != null)
+        {
+            int valueComparison = ComparedTo(current.Value, value);
+
+            if (valueComparison > 0) current = current.Left;
+            else if (valueComparison < 0) current = current.Right;
+            else return true;
+        }
+
+        return false;
     }
 
     public void DeleteMin()
@@ -112,109 +98,97 @@ public class BinarySearchTree<T> where T : IComparable<T>
         if (Root == null) return;
 
         Node parent = null;
-        var current = Root;
+        Node current = Root;
         while(current.Left != null)
         {
             parent = current;
             current = current.Left;
         }
 
-        if (parent == null) Root = null;
-        else parent.Left = current.Right; 
-    }
-
-    public BinarySearchTree<T> Search(T item)
-    {
-        var current = Root;
-        while(current != null)
-        {
-            T val = current.Value;
-            if(val.CompareTo(item) < 0)
-            {
-                current = current.Right;
-            }
-            else if(val.CompareTo(item) > 0)
-            {
-                current = current.Left;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return new BinarySearchTree<T>(current);
-    }
-
-    public IEnumerable<T> Range(T startRange, T endRange)
-    {
-        Queue<T> q = new Queue<T>();
-
-        Range(Root, q, startRange, endRange);
-
-        return q;
-    }
-
-    private void Range(Node node, Queue<T> q, T startRange, T endRange)
-    {
-        if (node == null) return;
-
-        int nodeInLower = startRange.CompareTo(node.Value);
-        int nodeInHigher = endRange.CompareTo(node.Value);
-       
-        if(nodeInLower < 0)
-        {
-            Range(node.Left, q, startRange, endRange);
-        }
-        if(nodeInLower <=0 && nodeInHigher >= 0)
-        {
-            q.Enqueue(node.Value);
-        }
-        if (nodeInHigher > 0)
-        {
-            Range(node.Right, q, startRange, endRange);
-        }
+        if (parent == null) Root = Root.Right;
+        else parent.Left = current.Right;
     }
 
     public void EachInOrder(Action<T> action)
     {
-        if(Root == null)
+        if (Root == null)
         {
+            Console.WriteLine("no root element");
             return;
         }
 
-        GoDeep(Root);
-        void GoDeep(Node root)
+        EachInOrder(Root, action);
+    }
+
+    private void EachInOrder(Node currentNode, Action<T> action)
+    {
+        if (currentNode.Left != null) EachInOrder(currentNode.Left, action);
+        action(currentNode.Value);
+        if (currentNode.Right != null) EachInOrder(currentNode.Right, action);
+    }
+
+    public void Print()
+    {
+        if (Root == null) return;
+
+        PrintRecursion(0, Root);
+    }
+
+    public IEnumerable<T> Range(T minValue, T maxValue)
+    {
+        Queue<T> q = new Queue<T>();
+
+        Range(Root, q, minValue, maxValue);
+
+        return q;
+    }
+
+    private void Range(Node root, Queue<T> q, T minValue, T maxValue)
+    {
+        if (root == null) return;
+        int lowValueComparison = ComparedTo(root.Value, minValue);
+        int highValueComparison = ComparedTo(root.Value, maxValue);
+
+        if (lowValueComparison > 0)
         {
-            if (root.Left != null)
-            {
-                GoDeep(root.Left);
-            }
-
-            action(root.Value);
-
-            if (root.Right != null)
-            {
-                GoDeep(root.Right);
-            }
-
-            
+            Range(root.Left, q, minValue, maxValue);
         }
+        if (lowValueComparison >= 0 && highValueComparison <= 0)
+        {
+            q.Enqueue(root.Value);
+        }
+        if(highValueComparison < 0)
+        {
+            Range(root.Right, q, minValue, maxValue);
+        }
+
+    }
+
+    private int ComparedTo(T a, T b)
+    {
+        return a.CompareTo(b);
+    }
+
+    private void PrintRecursion(int indent, Node currentNode)
+    {
+        Console.WriteLine(new String(' ', indent) + currentNode.Value);
+        if (currentNode.Left != null) PrintRecursion(indent + 2, currentNode.Left);
+        if (currentNode.Right != null) PrintRecursion(indent + 2, currentNode.Right);
     }
 
     private class Node
     {
-        public T Value { get; set; }
-        public Node Left { get; set; }
-        public Node Right { get; set; }
-
-        public Node(T val, 
-            Node left = null, Node right = null)
+        public Node(T value)
         {
-            Value = val;
-            Left = left;
-            Right = right;
+            if (value != null)
+            {
+                Value = value;
+            }
         }
+
+        public T Value;
+        public Node Left;
+        public Node Right;
     }
 }
 
@@ -222,24 +196,20 @@ public class Launcher
 {
     public static void Main(string[] args)
     {
-        //var bst = new BinarySearchTree<int>();
+        var bst = new BinarySearchTree<int>();
 
-        //bst.Insert(10);
-        //bst.Insert(5);
-        //bst.Insert(3);
-        //bst.Insert(1);
-        //bst.Insert(4);
-        //bst.Insert(8);
-        //bst.Insert(9);
-        //bst.Insert(37);
-        //bst.Insert(39);
-        //bst.Insert(45);
+        bst.Insert(10);
+        bst.Insert(5);
+        bst.Insert(3);
+        bst.Insert(1);
+        bst.Insert(4);
+        bst.Insert(8);
+        bst.Insert(9);
+        bst.Insert(37);
+        bst.Insert(39);
+        bst.Insert(45);
 
-        //// Act
-        //BinarySearchTree<int> result = bst.Search(5);
-        //List<int> nodes = new List<int>();
-        //result.EachInOrder(nodes.Add);
-
-        //Console.WriteLine(string.Join(" ", nodes));
+        bst.Print();
+        Console.WriteLine(string.Join(", ", bst.Range(4, 37)));
     }
 }
