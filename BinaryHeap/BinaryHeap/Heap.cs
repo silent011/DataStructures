4a212d0c8814cb4d99b6999edf4c068f343a878c
Silent-Era <junkyard@abv.bg>
2018-06-27 07:20:33 +0300

    sorting with heap

--- BinaryHeap/BinaryHeap/Heap.cs
+++ BinaryHeap/BinaryHeap/Heap.cs
@@ -4,6 +4,55 @@ public static class Heap<T> where T : IComparable<T>
 {
     public static void Sort(T[] arr)
     {
-        throw new NotImplementedException();
+        int n = arr.Length;
+        for (int i = n / 2; i >= 0; i--)
+        {
+            BubbleDown(arr, i, n);
+        }
+       
+        for(int i = n - 1; i > 0; i--)
+        {
+            Swap(arr, i, 0);
+            BubbleDown(arr, 0, i);
+        }
+    }
+
+    private static void BubbleDown(T[] arr, int current, int border)
+    {
+        while(current < border / 2)
+        {
+            int child = Left(current);
+            if(child + 1 < border && IsLess(arr,child, child + 1))
+            {
+                child = child + 1;
+            }
+
+            if (IsLess(arr,child, current)) break;
+
+            Swap(arr,current, child);
+            current = child;
+        }
+    }
+
+    private static void Swap(T[] arr, int current, int child)
+    {
+        T token = arr[current];
+        arr[current] = arr[child];
+        arr[child] = token;
+    }
+
+    private static bool IsLess(T[] arr, int index, int index2)
+    {
+        return arr[index].CompareTo(arr[index2]) < 0;
+    }
+
+    private static bool HasChild(T[] arr, int index)
+    {
+        return index < arr.Length;
+    }
+
+    private static int Left(int index)
+    {
+        return 2 * index + 1;
     }
 }
--- BinaryHeap/BinaryHeap/HeapExample.cs
+++ BinaryHeap/BinaryHeap/HeapExample.cs
@@ -4,20 +4,25 @@ public class HeapExample
 {
     static void Main()
     {
-        Console.WriteLine("Created an empty heap.");
-        var heap = new BinaryHeap<int>();
-        heap.Insert(5);
-        heap.Insert(8);
-        heap.Insert(1);
-        heap.Insert(3);
-        heap.Insert(12);
-        heap.Insert(-4);
-        heap.PrintHead();
-        Console.WriteLine("Heap elements (max to min):");
-        while (heap.Count > 0)
-        {
-            var max = heap.Pull();
-            Console.WriteLine(max);
-        }
+        //Console.WriteLine("Created an empty heap.");
+        //var heap = new Heap<int>();
+        //heap.Insert(5);
+        //heap.Insert(8);
+        //heap.Insert(1);
+        //heap.Insert(3);
+        //heap.Insert(12);
+        //heap.Insert(-4);
+        //heap.PrintHeap();
+        //Console.WriteLine("Heap elements (max to min):");
+        //while (heap.Count > 0)
+        //{
+        //    var max = heap.Pull();
+        //    Console.WriteLine(max);
+        //}
+
+        int[] arr = { 5, 8, 1, 3, 12, -4 };
+        Heap<int>.Sort(arr);
+
+        Console.WriteLine(string.Join(" ", arr));
     }
 }
