29e415e542b39868ab76c6c2de81eb95d0f84c8a
Silent-Era <junkyard@abv.bg>
2018-07-02 18:10:02 +0300

    Implementing A* algorithm using a priority queue

---
+++ AStar/AStar/AStar.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AStar
+{
+    private Node Start;
+    private Node Goal;
+    private PriorityQueue<Node> Path = new PriorityQueue<Node>();
+    private char[,] Map;
+    private Dictionary<Node, Node> Parents;
+    private Dictionary<Node, int> GCosts;
+
+    public AStar(char[,] map)
+    {
+        Map = map;
+        Parents = new Dictionary<Node, Node>();
+        GCosts = new Dictionary<Node, int>();
+    }
+
+
+    public static int GetH(Node currentPos, Node goalPos)
+    {
+        int XDiff = Math.Abs(currentPos.Col - goalPos.Col);
+        int YDiff = Math.Abs(currentPos.Row - goalPos.Row);
+
+        return XDiff + YDiff;
+    }
+
+    public IEnumerable<Node> GetPath(Node start, Node goal)
+    {
+        if (start == null || goal == null) throw new InvalidOperationException("goal or start missing");
+
+        if (Parents.ContainsKey(goal) && Parents.ContainsKey(start))
+        {
+            return FillAlreadyKnownPath(start, goal);
+        }
+
+        Start = start;
+        Goal = goal;
+        Parents = new Dictionary<Node, Node>();
+        GCosts = new Dictionary<Node, int>();
+        Path = new PriorityQueue<Node>();
+
+        Stack<Node> shortestPath = new Stack<Node>();
+        Parents.Add(Start, null);
+        GCosts.Add(Start, 0);
+        Path.Enqueue(Start);
+
+        Node endOfPath = SearchShortestPath();
+     
+        if (endOfPath == null)
+        {
+            shortestPath.Push(Start);
+            return shortestPath;
+        }
+
+        Fill(shortestPath, endOfPath);
+
+        return shortestPath;
+    }
+
+    private IEnumerable<Node> FillAlreadyKnownPath(Node start, Node goal)
+    {
+        Stack<Node> resultPath = new Stack<Node>();
+        resultPath.Push(goal);
+        Node current = Parents[goal];
+        while(current != null)
+        {
+            resultPath.Push(current);
+            if (current.Equals(start)) break;
+            current = Parents[current];
+        }
+
+        return resultPath;
+    }
+
+    private void Fill(Stack<Node> shortestPath, Node current)
+    {
+        if (current == null) return;
+
+        shortestPath.Push(current);
+        Fill(shortestPath, Parents[current]);
+    }
+
+    private Node SearchShortestPath()
+    {
+        while(Path.Count > 0)
+        {
+            Node current = Path.Dequeue();
+            if (current.Equals(Goal)) return current;
+
+            GetNeighbours(current);
+        }
+
+        return null;
+    }
+
+    private void GetNeighbours(Node current)
+    {
+        int x = current.Col;
+        int y = current.Row;
+        GetUpper(current, x, y);
+        GetRight(current, x, y);
+        GetDowner(current, x, y);
+        GetLeft(current, x, y);
+    }
+
+    private void GetLeft(Node current, int x, int y)
+    {
+        if (x - 1 >= 0 && !Map.GetValue(y, x - 1).Equals('W'))
+        {
+            Node left  = new Node(y, x - 1);
+            if (GCosts.ContainsKey(left)) return;
+
+            Parents.Add(left, current);
+            GCosts.Add(left, GCosts[current] + 1);
+
+            left.F = CalcFCost(left, current);
+            Path.Enqueue(left);
+        }
+    }
+
+    private void GetDowner(Node current, int x, int y)
+    {
+        if (y + 1 < Map.GetLength(0) && !Map.GetValue(y+1, x).Equals('W'))
+        {
+            Node down = new Node(y + 1, x);
+            if (GCosts.ContainsKey(down)) return;
+
+            Parents.Add(down, current);
+            GCosts.Add(down, GCosts[current] + 1);
+
+            down.F = CalcFCost(down, current);
+            Path.Enqueue(down);
+        }
+    }
+
+    private int CalcFCost(Node target, Node current)
+    {
+        return GCosts[current] + GetH(current, Goal);
+    }
+
+    private void GetRight(Node current, int x, int y)
+    {
+        if (x + 1 < Map.GetLength(1) && !Map.GetValue(y, x + 1).Equals('W'))
+        {
+            Node right = new Node(y, x + 1);
+            if (GCosts.ContainsKey(right)) return;
+
+            Parents.Add(right, current);
+            GCosts.Add(right, GCosts[current] + 1);
+
+            right.F = CalcFCost(right, current);
+            Path.Enqueue(right);
+        }
+    }
+
+    private void GetUpper(Node current, int x, int y)
+    {
+        if (y - 1 >= 0 && !Map.GetValue(y - 1, x).Equals('W'))
+        {
+            Node up = new Node(y - 1, x);
+            if (GCosts.ContainsKey(up)) return;
+
+            Parents.Add(up, current);
+            GCosts.Add(up, GCosts[current] + 1);
+
+            up.F = CalcFCost(up, current);
+            Path.Enqueue(up);
+        }
+    }
+}
+
---
+++ AStar/AStar/PriorityQueue.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PriorityQueue<T> where T : IComparable<T>
+{
+    private List<T> heap;
+
+    public PriorityQueue()
+    {
+        this.heap = new List<T>();
+    }
+
+    public int Count
+    {
+        get { return this.heap.Count; }
+    }
+
+    //public bool Contains(T elem)
+    //{
+    //    int len = heap.Count;
+    //    for(int i = (len - 1)/2; i > 0; i--)
+    //    {
+    //        if (heap[i].CompareTo(elem) > 0) continue;
+
+    //        if (Left(i).CompareTo(elem) == 0 || Right(i).CompareTo(elem) == 0) return true;
+    //    }
+
+    //    return false;
+    //}
+
+    public void Enqueue(T item)
+    {
+        this.heap.Add(item);
+        this.HeapifyUp(this.heap.Count - 1);
+    }
+
+    public T Peek()
+    {
+        return this.heap[0];
+    }
+
+    public T Dequeue()
+    {
+        if (this.Count <= 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = this.heap[0];
+
+        this.Swap(0, this.heap.Count() - 1);
+        this.heap.RemoveAt(this.heap.Count() - 1);
+        this.HeapifyDown(0);
+
+        return item;
+    }
+
+    public void DecreaseKey(T item)
+    {
+        int len = heap.Count;
+        for(int i = (len - 1)/2; i >= 0; i--)
+        {
+            if (heap[i].Equals(item))
+            {
+                HeapifyUp(i);
+                break;
+            } 
+            else if (HasChild(Left(i)) && heap[Left(i)].Equals(item))
+            {
+                HeapifyUp(Left(i));
+                break;
+            }
+            else if (HasChild(Right(i)) && heap[Right(i)].Equals(item))
+            {
+                HeapifyUp(Right(i));
+                break;
+            }
+        }
+    }
+
+    private void HeapifyUp(int index)
+    {
+        while (index > 0 && IsLess(index, Parent(index)))
+        {
+            this.Swap(index, Parent(index));
+            index = Parent(index);
+        }
+    }
+
+    private void HeapifyDown(int index)
+    {
+        while (index < this.heap.Count / 2)
+        {
+            int child = Left(index);
+            if (HasChild(child + 1) && IsLess(child + 1, child))
+            {
+                child = child + 1;
+            }
+
+            if (IsLess(index, child))
+            {
+                break;
+            }
+
+            this.Swap(index, child);
+            index = child;
+        }
+    }
+
+    private bool HasChild(int child)
+    {
+        return child < this.heap.Count;
+    }
+
+    private static int Parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+
+    private static int Left(int index)
+    {
+        return 2 * index + 1;
+    }
+
+    private static int Right(int index)
+    {
+        return Left(index) + 1;
+    }
+
+    private bool IsLess(int a, int b)
+    {
+        return this.heap[a].CompareTo(this.heap[b]) < 0;
+    }
+
+    private void Swap(int a, int b)
+    {
+        T temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+}
