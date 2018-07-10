ef1532db8e6422ca528d0b5502b106accf9818f6
Silent-Era <junkyard@abv.bg>
2018-07-11 00:25:52 +0300

    avl deletemin and delete methods implementation

---
+++ AVLExercise/AVLTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Node<T> where T : IComparable<T>
+{
+    public T Value;
+    public Node<T> Left;
+    public Node<T> Right;
+    public int Height;
+
+    public Node(T value)
+    {
+        this.Value = value;
+        this.Height = 1;
+    }
+
+    public Node<T> Copy()
+    {
+        Node<T> paste = new Node<T>(Value);
+        paste.Right = Right;
+        paste.Left = Left;
+        return paste;
+    }
+}
---
+++ AVLExercise/AVLTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AVL<int> avl = new AVL<int>();
+        for (int i = 1; i < 10; i++)
+        {
+            avl.Insert(i);
+        }
+
+        //avl.Test();
+
+        avl.Delete(4);
+        var root = avl.Root;
+        root.Left.Left.Height = 4; // 1
+        root.Left.Right.Height = 4; // 3
+        root.Right.Left.Right.Height = 4; // 7
+        root.Right.Right.Height = 4; // 9
+
+        // Nodes of height 2
+        root.Left.Height = 4; // 2
+        root.Right.Left.Height = 4; //6
+
+        // Nodes of height 3
+        root.Right.Height = 4;// 8
+
+        // Nodes of height 4
+        root.Height = 4; // 5
+
+        Console.WriteLine();
+    }
+}
